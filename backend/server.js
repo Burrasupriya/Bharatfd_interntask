db3f7a65f61b238d18c07ae3434a86f421372290
Burrasupriya <141541003+Burrasupriya@users.noreply.github.com>
2025-02-02 09:56:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,14 @@ const faqRoutes = require("./routes/faqs");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors()); // Enable Cross-Origin Resource Sharing
-app.use(express.json()); // Parse JSON request bodies
 
-// Routes
+app.use(cors()); 
+app.use(express.json()); 
+
+
 app.use("/api/faqs", faqRoutes);
 
-// Start Server
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
